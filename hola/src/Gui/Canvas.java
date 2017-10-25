5330e259e144f166cf826c67e8e58a6d0e2599bd
modelos1 <30787127+modelos1@users.noreply.github.com>
2017-10-25 10:42:05 -0500

    Add files via upload

---
+++ hola/src/Gui/Canvas.java
@@ -0,0 +1,47 @@
+package Gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+
+public class Canvas extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Canvas frame = new Canvas();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Canvas() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		java.awt.Canvas canvas = new java.awt.Canvas();
+		canvas.setForeground(Color.LIGHT_GRAY);
+		canvas.setBounds(10, 10, 414, 242);
+		contentPane.add(canvas);
+	}
+}
